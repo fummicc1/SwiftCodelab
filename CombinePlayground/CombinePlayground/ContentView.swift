47c46e93c91e88716726795bc92fb5505ce8065a
fummicc1 <fumiya989@gmail.com>
2020-03-14 03:44:04 +0900

    add: CombinePlayground

---
+++ CombinePlayground/CombinePlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombinePlayground
+//
+//  Created by Fumiya Tanaka on 2020/03/14.
+//  Copyright © 2020 Fumiya Tanaka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
