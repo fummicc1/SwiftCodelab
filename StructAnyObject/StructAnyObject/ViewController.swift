cd6287558f4d752e07a84a69bc4dfd847300386c
fummicc1 <fumiya989@gmail.com>
2020-03-15 06:26:36 +0900

    add: code

--- StructAnyObject/StructAnyObject/ViewController.swift
+++ StructAnyObject/StructAnyObject/ViewController.swift
@@ -22,6 +22,24 @@ class Util {
     static func isClass(about target: Any) -> Bool {
         type(of: target) is AnyObject.Type
     }
+    
+    static func unwrap(_ target: Any) -> Any? {
+        let mirroredArticle = Mirror(reflecting: target)
+        guard mirroredArticle.displayStyle == .some(.optional) else {
+            return target
+        }
+        return mirroredArticle.children.first?.value
+    }
+}
+
+protocol OptionalKind {
+    var optional: Any? { get }
+}
+
+extension Optional: OptionalKind {
+    var optional: Any? {
+        self.map { $0 }
+    }
 }
 
 class ViewController: UIViewController {
@@ -33,6 +51,9 @@ class ViewController: UIViewController {
         let value = Util.asClass(about: article)
         print(value) // not nil.
         print(Util.isClass(about: article)) // expected
+        let optionalArticle = Optional(article) as Any
+        print(Util.unwrap(optionalArticle)!) // possible to force unwrap
+        print(optionalArticle) // same as above
+        print(value.optional)
     }
 }
-
