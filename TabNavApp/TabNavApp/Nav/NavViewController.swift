e6583d07f284cdbce0b536e68033dcc6d1beec8d
fummicc1 <fumiya989@gmail.com>
2020-03-15 21:39:42 +0900

    add: create TabNavApp

---
+++ TabNavApp/TabNavApp/Nav/NavViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NavViewController.swift
+//  TabNavApp
+//
+//  Created by Fumiya Tanaka on 2020/03/15.
+//  Copyright © 2020 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class NavViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ TabNavApp/TabNavApp/Tab/TabViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabNavApp
+//
+//  Created by Fumiya Tanaka on 2020/03/15.
+//  Copyright © 2020 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class TabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
