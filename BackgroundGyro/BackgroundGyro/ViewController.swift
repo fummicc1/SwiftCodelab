85f3dd8ea59e99e6574336aa93cb5ace7b634a43
fummicc1 <fumiya989@gmail.com>
2020-03-18 00:56:38 +0900

    add: init BackgroundGyro

---
+++ BackgroundGyro/BackgroundGyro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundGyro
+//
+//  Created by Fumiya Tanaka on 2020/03/18.
+//  Copyright © 2020 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
