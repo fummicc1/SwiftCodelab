e41edf68160a9b58c84c4f2eef1c067b449170d5
fummicc1 <fumiya989@gmail.com>
2020-04-01 08:54:48 +0900

    add: PracticeRxTests

---
+++ PracticeRxTests/PracticeRxTests/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeRxTests
+//
+//  Created by Fumiya Tanaka on 2020/04/01.
+//  Copyright © 2020 Fumiya Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
