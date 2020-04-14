61f90f638ed5749eb9edeec1b3b30beb114c834d
fummicc1 <fumiya.tennis1@gmail.com>
2020-04-14 15:08:12 +0900

    WIP: save and fetch item from Keychain.

--- KeychainBundleIDSample/KeychainBundleIDSample/ViewController.swift
+++ KeychainBundleIDSample/KeychainBundleIDSample/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import Security
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction private func addItem() {
+        let account: String = "account"
+        let password: Data = "password".data(using: .utf8)!
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: Bundle.main.bundleIdentifier!,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: password
+        ]
+        let status = SecItemAdd(query as CFDictionary, nil)
+        assert(status == errSecSuccess)
+        print("succeed adding")
+    }
+    
+    @IBAction private func fetchItem() {
+        let query: [String: Any] = [
+            kSecClass as String : kSecClassGenericPassword,
+            kSecAttrService as String: Bundle.main.bundleIdentifier!,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+            kSecReturnAttributes as String: true,
+            kSecReturnData as String: true
+        ]
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        assert(status != errSecItemNotFound)
+        assert(status == errSecSuccess)
+        guard let existingItem = item as? [String: Any],
+            let passwordData = existingItem[kSecValueData as String] as? Data,
+            let password = String(data: passwordData, encoding: String.Encoding.utf8),
+            let account = existingItem[kSecAttrAccount as String] as? String else {
+                assert(false)
+                return
+        }
+        assert(account == "account")
+        assert(password == "password")
+        print("succeed fetching")
+    }
+    
+    @IBAction private func deleteItem() {
+        
+    }
+    
 }
 
